3e0c93e8baf84ae2da208e140f365063c38e80e9
Ronis <ronaldo.luizcp@gmail.com>
2022-02-05 01:32:16 -0300

    Alteracao de diretorio de origem, remocao de propriedade readonly e exclusao da pasta copia

--- ServicoAutoBackup.cs
+++ ServicoAutoBackup.cs
@@ -16,10 +16,39 @@ namespace AutoBackup
         public void Start()
         {
            
-            string pathOrigem = @"E:\Ronis";
-            string pathCopy = @"E:\C_Ronis";
+            string pathOrigem = @"C:\Users\ronal\Desktop\MyFolder\Ronis";
+            string pathCopy = @"C:\Users\ronal\Desktop\MyFolder\C_Ronis";
+            DirectoryInfo parentDirectoryInfo = new DirectoryInfo(@"C:\Users\ronal\Desktop\MyFolder");
+
+            ClearReadOnly(parentDirectoryInfo);
+            var pathCopyInfo = new DirectoryInfo(pathCopy);
+            if (pathCopyInfo.Exists)
+            {
+                DeleteFolder(pathCopy);
+            }       
             CopyDirectory(pathOrigem, pathCopy, true);
-            ZiparEnviarPasta();
+            ZipAndSend();
+        }
+
+        private void ClearReadOnly(DirectoryInfo parentDirectory)
+        {
+            if (parentDirectory != null)
+            {
+                parentDirectory.Attributes = FileAttributes.Normal;
+                foreach (FileInfo fi in parentDirectory.GetFiles())
+                {
+                    fi.Attributes = FileAttributes.Normal;
+                }
+                foreach (DirectoryInfo di in parentDirectory.GetDirectories())
+                {
+                    ClearReadOnly(di);
+                }
+            }
+        }
+        static void DeleteFolder(string pathCopy)
+        {
+            Directory.Delete(pathCopy, true);
+            System.Threading.Thread.Sleep(10000);
         }
 
         static void CopyDirectory(string sourceDir, string destinationDir, bool recursive)
@@ -27,7 +56,7 @@ namespace AutoBackup
 
             var dir = new DirectoryInfo(sourceDir);
             if (!dir.Exists)
-                throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
+                throw new DirectoryNotFoundException($"Arquivo de origem não existe: {dir.FullName}");
             DirectoryInfo[] dirs = dir.GetDirectories();
             Directory.CreateDirectory(destinationDir);
             foreach (FileInfo file in dir.GetFiles())
@@ -47,26 +76,29 @@ namespace AutoBackup
             
         }
 
-        static void ZiparEnviarPasta()
+        static void ZipAndSend()
         {
             Thread.CurrentThread.CurrentCulture = new CultureInfo("pt-BR", false);
             DateTime localDate = DateTime.Now;
             string dataQuaseFormatada = localDate.ToString("s");
             string dataFormatada = dataQuaseFormatada.Substring(0, dataQuaseFormatada.IndexOf('T'));
 
-            string zip = @$"E:\Ronis-{dataFormatada}.zip";
-            string pathCopy = @"E:\C_Ronis";
-            string pathDestino = @$"C:\Users\ronal\OneDrive\AutobackUp-{dataFormatada}.zip";
+            //string zip = @$"E:\Ronis-{dataFormatada}.zip";
+            string zip = @$"C:\Users\ronal\Desktop\MyFolder\Ronis-{dataFormatada}.zip";
+            string pathCopy = @"C:\Users\ronal\Desktop\MyFolder\C_Ronis";
+            string pathDestino = @$"C:\Users\ronal\OneDrive\Autobackup-{dataFormatada}.zip";
 
             ZipFile.CreateFromDirectory(pathCopy, zip);
             Microsoft.VisualBasic.FileIO.FileSystem.MoveFile(zip, pathDestino);
+            
+
 
 
         }
 
         public void Stop()
             {
-                
+            
             }
             
     }
